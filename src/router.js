d9ac5182270891dac9cd61bd5af8c9d3ba3435ef
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2024-05-05 21:33:32 -0500

    Meals Details working

--- src/router.js
+++ src/router.js
@@ -2,12 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
 import MealsByName from './views/MealsByName.vue'
+import MealsByLetter from './views/MealsByLetter.vue'
 import MealsByIngredients from './views/MealsByIngredients.vue'
 import MealDetails from './views/MealDetails.vue'
 import Ingredients from './views/Ingredients.vue'
 
 const routes = [
-   { path: '/meal-search/', name: 'Home', component: HomePage },
+  { path: '/meal-search/', name: 'Home', component: HomePage },
   { path: '/meal-search/by-name/:name?', name: 'MealsByName', component: MealsByName },
   { path: '/meal-search/by-letter/:letter?', name: 'MealsByLetter', component: MealsByLetter },
   { path: '/meal-search/by-ingredient/:ingredient?', name: 'MealsByIngredients', component: MealsByIngredients },
